9f57f6a59c6b688e6c28297a4b8d23a07806edeb
MasterSais <evgensvvv@gmail.com>
2020-03-22 17:23:35 +0300

    Compiler: Composer

--- compiler/composer.js
+++ compiler/composer.js
@@ -1,9 +1,124 @@
-import { LSB, RSB, LRB, RRB, LFB, RFB, DLM, GT, LT, EQ, VL } from './lexemes.js';
+/* eslint-disable no-undef */
+
+import { date, is, length, number, object2, string } from '../dist/es/index.min.js';
+import { EQ, GT, LFB, LRB, LSB, LT, RFB, RRB, RSB, VL } from './lexemes.js';
+
+const validatorBase = new Map([
+  ['object', { callee: object2 }],
+  ['date', { callee: date }],
+  ['compare', { callee: is }],
+  ['number', { callee: number }],
+  ['string', { callee: string }],
+  ['length', { callee: length }]
+]);
 
 export const composer = (lexemes) => {
+  const validatorsTree = [];
+  const stack = [validatorsTree];
+  const states = [];
+  
+  let validators = validatorsTree;
+
+  lexemes = lexemes.slice(1);
+
   for (const lexeme of lexemes) {
+    let state = states[states.length - 1];
+
+    if ([GT.code, LT.code, EQ.code].includes(lexeme.code)) {
+      if (state === 'comparator') {
+        validators[validators.length - 1] += lexeme.value;
+      } else {
+        validators.push(lexeme.value);
+        states.push('comparator');
+      }
+
+      continue;
+    } else {
+      if (state === 'comparator') {
+        states.pop();
+      }
+    }
+
+    state = states[states.length - 1];
+
     if (lexeme.code === VL.code) {
-      console.log(lexeme.value);
+      if (state === 'field') {
+        validators.push('f_' + lexeme.value);
+
+        states.pop();
+
+        continue;
+      }
+
+      if (state === 'injection') {
+        validators[validators.length - 1] += lexeme.value;
+
+        continue;
+      }
+
+      if (state === 'params') {
+        validators.push('p_' + lexeme.value);
+
+        continue;
+      }
+
+      const validator = validatorBase.get(lexeme.value);
+
+      if (validator) {
+        validators.push('v_' + lexeme.value);
+      }
+
+      continue;
+    }
+
+    if (lexeme.code === LSB.code) {
+      states.push('validator');
+
+      states.push('field');
+    }
+
+    if (lexeme.code === RSB.code) {
+      states.pop();
+    }
+
+    if (lexeme.code === LRB.code) {
+      validators.push([]);
+
+      stack.push(validators[validators.length - 1]);
+
+      validators = validators[validators.length - 1];
+
+      states.push('params');
+
+      continue;
+    }
+
+    if (lexeme.code === RRB.code) {
+      stack.pop();
+
+      validators = stack[stack.length - 1];
+
+      states.pop();
+
+      continue;
+    }
+
+    if (lexeme.code === LFB.code) {
+      states.push('injection');
+
+      validators.push(lexeme.value);
+
+      continue;
+    }
+
+    if (lexeme.code === RFB.code) {
+      states.pop();
+
+      validators[validators.length - 1] += lexeme.value;
+
+      continue;
     }
   }
+
+  console.log(validatorsTree);
 };
--- compiler/index.js
+++ compiler/index.js
@@ -1,14 +1,14 @@
-import { lexicalAnalyzer } from './lexical-analyzer.js';
-import { semanticAnalyzer } from './semantic-analyzer.js';
 import { composer } from './composer.js';
 import { lexemeBase } from './lexemes.js';
+import { lexicalAnalyzer } from './lexical-analyzer.js';
+import { semanticAnalyzer } from './semantic-analyzer.js';
 import { semanticRules } from './semantic-rules.js';
 
 const test = (
   `
     [object(
-      [createdAt : date : compare(>{0})]
-      [updatedAt : date : compare(>=createdAt)]
+      [date : date : compare(>{0})]
+      [updatedAt : date : compare(>=date)]
       [deletedAt : date : compare(>=updatedAt)]
       [user : object( 
         [id : number : compare(>=0)]
@@ -20,11 +20,9 @@ const test = (
 
 const lexemes = lexicalAnalyzer(test, lexemeBase);
 
-console.log(lexemes.map(({ value, code }) => ({ code, value })));
-
 semanticAnalyzer(semanticRules, 0, lexemes);
 
-console.log(composer(lexemes));
+composer(lexemes);
 
 export const template = (input) => {
   const lexemes = lexicalAnalyzer(input, lexemeBase);
