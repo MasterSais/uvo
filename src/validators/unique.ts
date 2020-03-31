2a2be6c18f0a9c21b84bb06a5b8c7814b8020e64
MasterSais <evgensvvv@gmail.com>
2020-03-31 19:35:22 +0300

    Unique refactoring

--- src/validators/unique.ts
+++ src/validators/unique.ts
@@ -5,30 +5,32 @@ import { applyError, isArray, isFunction, setMetaPath, setMetaValidator } from '
 /**
  * {@link docs/validators/unique}
  */
-export const unique = <T>(field?: string | number | ((value: T) => any), error?: Error): Validator<Array<T>> => (
-  (data: Array<T>, onError?: ErrorCallback, meta?: MetaData): Array<T> => {
-    const valuesMap = {};
+export const unique = <T>(field?: string | number | ((value: T) => any), error?: Error): Validator<Array<T>> => {
+  const isMapperFunction = isFunction(field);
 
-    const isMapperFunction = isFunction(field);
+  const mapper = (isMapperFunction ? field : (value: T) => (field ? value[field as string] : value)) as Function;
 
-    const mapper = (isMapperFunction ? field : (value: T) => (field ? value[field as string] : value)) as Function;
+  return (
+    (data: Array<T>, onError?: ErrorCallback, meta?: MetaData): Array<T> => {
+      const valuesMap = {};
 
-    meta = setMetaValidator(meta, V_UQ, [data]);
+      meta = setMetaValidator(meta, V_UQ, [data]);
 
-    return isArray(data)
-      ? (
-        data.map((item, index) => {
-          const value = mapper(item);
+      return isArray(data)
+        ? (
+          data.map((item, index) => {
+            const value = mapper(item);
 
-          return valuesMap[value] === undefined
-            ? (
-              valuesMap[value] = index, item
-            )
-            : (
-              applyError(error, onError, setMetaPath(meta, (field && !isMapperFunction) ? [index, field] : [index])), null
-            );
-        })
-      )
-      : applyError(error, onError, meta);
-  }
-);
+            return valuesMap[value] === undefined
+              ? (
+                valuesMap[value] = index, item
+              )
+              : (
+                applyError(error, onError, setMetaPath(meta, (field && !isMapperFunction) ? [index, field] : [index])), null
+              );
+          })
+        )
+        : applyError(error, onError, meta);
+    }
+  );
+};
