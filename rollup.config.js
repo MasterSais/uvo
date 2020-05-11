eb5afebde888e937d0fbee80928163110f31af78
MasterSais <evgensvvv@gmail.com>
2020-05-11 22:32:58 +0300

    v3.0.4

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import { resolve } from 'path';
 import del from 'rollup-plugin-delete';
 import { terser } from 'rollup-plugin-terser';
 import typescript2 from 'rollup-plugin-typescript2';
-import copy from 'rollup-plugin-copy'
 
 const tersetOptions = {
   module: true,
@@ -43,6 +42,16 @@ const module = outDir => ({
   }
 });
 
+const typesLink = (outDir, typesDir) => ({
+  name: 'typesLink',
+  buildEnd: () => {
+    writeFileSync(resolve(outDir + '/package.json'), JSON.stringify(
+      {
+        typings: typesDir + '/index.d.ts'
+      }
+    ));
+  }
+});
 
 const types = (input, outDir) => ({
   input,
@@ -54,6 +63,7 @@ const types = (input, outDir) => ({
         outDir + '/index.d.ts',
         outDir + '/esm/*',
         outDir + '/cjs/*',
+        outDir + '/es5/*',
         outDir + '/umd/*'
       ]
     }),
@@ -112,39 +122,56 @@ const bundle = (input, outDir, createModule) => ({
   ]
 });
 
+const es5Bundle = (input, outDir = '.') => ({
+  input,
+  plugins: [
+    typescript2({
+      tsconfigOverride: {
+        compilerOptions: {
+          target: 'es5'
+        }
+      }
+    })
+  ],
+  output: [
+    {
+      file: outDir + '/es5/index.js',
+      format: 'cjs'
+    },
+    {
+      file: outDir + '/es5/index.min.js',
+      format: 'cjs',
+      plugins: [terser(tersetOptions)],
+    },
+  ]
+});
+
+const copyTypes = (input, inDir, outDir = '.') => ({
+  input: input,
+  plugins: [typesLink(outDir + '/es5', inDir)],
+  output: {
+    file: input
+  }
+});
+
 export default [
   types('dist/index.ts', 'dist'),
   types('template/index.ts', 'template'),
   types('extended/index.ts', 'extended'),
   types('extended-template/index.ts', 'extended-template'),
+
   bundle('src/base-api/index.ts', 'dist'),
   bundle('src/templating-api/template.ts', 'template', true),
   bundle('src/base-api/extensions/index.ts', 'extended', true),
   bundle('src/templating-api/extensions/index.ts', 'extended-template', true),
-  {
-    input: 'src/base-api/index.ts',
-    plugins: [
-      typescript2({
-        tsconfigOverride: {
-          compilerOptions: {
-            target: 'es5'
-          }
-        }
-      }),
-      copy({
-        targets: [
-          { src: 'dist/index.d.ts', dest: 'es5' },
-        ]
-      })
-    ],
-    output: [
-      {
-        file: 'es5/index.js'
-      },
-      {
-        file: 'es5/index.min.js',
-        plugins: [terser(tersetOptions)],
-      },
-    ]
-  }
+
+  es5Bundle('src/base-api/index.ts'),
+  es5Bundle('src/templating-api/template.ts', 'template'),
+  es5Bundle('src/base-api/extensions/index.ts', 'extended'),
+  es5Bundle('src/templating-api/extensions/index.ts', 'extended-template'),
+
+  copyTypes('es5/index.js', '../dist'),
+  copyTypes('template/es5/index.js', '..', 'template'),
+  copyTypes('extended/es5/index.js', '..', 'extended'),
+  copyTypes('extended-template/es5/index.js', '..', 'extended-template'),
 ];
